c9d264357c94539dd95e2f549c4eeb9b4961f409
sivadhanush22 <60962312+sivadhanush22@users.noreply.github.com>
2025-01-18 23:43:41 +0530

    Update HELLO_DDL.sql

--- ddl_files/HELLO_DDL.sql
+++ ddl_files/HELLO_DDL.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `tables_ods.hello` (
-  `Updated_Time` DATE,,,,,,,
-  `Date` DATE,,,,,,,
+  `Updated_Time` DATE,,,,,,,,
+  `Date` DATE,,,,,,,,
   Metauid STRING,
   Metains TIMESTAMP
 );

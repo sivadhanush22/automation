eb15477c45008688598914112b18318f0a3456bb
sivadhanush22 <60962312+sivadhanush22@users.noreply.github.com>
2025-01-19 01:53:08 +0530

    Update test_DDL.sql

--- ddl_files/test_DDL.sql
+++ ddl_files/test_DDL.sql
@@ -1,5 +1,6 @@
-CREATE TABLE `tables_ods.test` (
+CREATE TABLE `tables_ods.Test` (
   `id` NUMERIC,
+  `created_ts` timestamp without time zone,
   `createdTs` DATETIME,
   Metauid STRING,
   Metains TIMESTAMP

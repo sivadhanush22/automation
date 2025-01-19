15b225bab3cfeb2243d7869b2503c4f7b99721ff
sivadhanush22 <60962312+sivadhanush22@users.noreply.github.com>
2025-01-19 19:11:44 +0530

    Update Test_DDL.sql

--- ddl_files/Test_DDL.sql
+++ ddl_files/Test_DDL.sql
@@ -1,4 +1,7 @@
-CREATE TABLE tables_ods.Test (
-  Metauid STRING,
-  Metains TIMESTAMP
+CREATE TABLE `tables_ods.Test_DDL` (
+  `CREATE` TABLE,
+  `Metauid` STRING,
+  `Metains` TIMESTAMP,
+  `Metauid` STRING,
+  `Metains` TIMESTAMP
 );
